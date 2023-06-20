623326c003bd45d1864f813505550ea720290472
011011000110110001110000 <106524046011011000110110001110000@users.noreply.github.com>
2023-06-20 17:19:34 +0200

    Fix test formatting

--- src/test/java/ReflectionUtilsTest.java
+++ src/test/java/ReflectionUtilsTest.java
@@ -7,31 +7,31 @@ import java.util.Map;
 import java.util.Set;
 
 public class ReflectionUtilsTest {
-	@Test
-	void testInit() {
-		Assertions.assertDoesNotThrow(
-			() -> {
-				Class<?> classReflectionUtils = null;
-				try {
-					classReflectionUtils = Class.forName("me.lollopollqo.sus.injection.util.ReflectionUtils");
-				} catch (ClassNotFoundException cnfe) {
-					// This should never happen
-				}
-				return classReflectionUtils;
-			}
-		);
-	}
+    @Test
+    void testInit() {
+        Assertions.assertDoesNotThrow(
+            () -> {
+                Class<?> classReflectionUtils = null;
+                try {
+                    classReflectionUtils = Class.forName("me.lollopollqo.sus.injection.util.ReflectionUtils");
+                } catch (ClassNotFoundException cnfe) {
+                    // This should never happen
+                }
+                return classReflectionUtils;
+            }
+        );
+    }
 
-	@Test
-	void testAddOpens() {
-		Assertions.assertDoesNotThrow(
-				() -> {
-					Class<?> reflectionClass = Class.forName("jdk.internal.reflect.Reflection");
-					Module javaBaseModule = reflectionClass.getModule();
-					ReflectionUtils.addOpens(javaBaseModule, "jdk.internal.reflect");
-					Method method = reflectionClass.getDeclaredMethod("registerFilter", Map.class, Class.class, Set.class);
-					method.setAccessible(true);
-				}
-		);
-	}
+    @Test
+    void testAddOpens() {
+        Assertions.assertDoesNotThrow(
+            () -> {
+                Class<?> reflectionClass = Class.forName("jdk.internal.reflect.Reflection");
+                Module javaBaseModule = reflectionClass.getModule();
+                ReflectionUtils.addOpens(javaBaseModule, "jdk.internal.reflect");
+                Method method = reflectionClass.getDeclaredMethod("registerFilter", Map.class, Class.class, Set.class);
+                method.setAccessible(true);
+            }
+        );
+    }
 }
