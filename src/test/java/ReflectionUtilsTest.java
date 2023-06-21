0e75f600550ec1936fcf8c0768b30b01b3d0ccfc
011011000110110001110000 <106524046011011000110110001110000@users.noreply.github.com>
2023-06-21 16:57:23 +0200

    Remove ReflectionUtilsTest.testUsageWithNoExplicitInitialization

--- src/test/java/ReflectionUtilsTest.java
+++ src/test/java/ReflectionUtilsTest.java
@@ -1,10 +1,5 @@
-import me.lollopollqo.sus.injection.util.ReflectionUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Set;
 
 public class ReflectionUtilsTest {
     @Test
@@ -18,19 +13,6 @@ public class ReflectionUtilsTest {
                     // This should never happen
                 }
                 return classReflectionUtils;
-            }
-        );
-    }
-
-    @Test
-    void testAddOpens() {
-        Assertions.assertDoesNotThrow(
-            () -> {
-                Class<?> reflectionClass = Class.forName("jdk.internal.reflect.Reflection");
-                Module javaBaseModule = reflectionClass.getModule();
-                ReflectionUtils.addOpens(javaBaseModule, "jdk.internal.reflect");
-                Method method = reflectionClass.getDeclaredMethod("registerFilter", Map.class, Class.class, Set.class);
-                method.setAccessible(true);
             }
         );
     }
