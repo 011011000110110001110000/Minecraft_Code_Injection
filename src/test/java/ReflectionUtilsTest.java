8e9f368f1a6dd576a4a1d04069088e1adc0da52b
011011000110110001110000 <106524046011011000110110001110000@users.noreply.github.com>
2023-07-03 01:18:16 +0200

    Add testChangeFieldType

--- src/test/java/ReflectionUtilsTest.java
+++ src/test/java/ReflectionUtilsTest.java
@@ -2,6 +2,7 @@ import me.lollopollqo.sus.injection.util.ReflectionUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 import java.util.Map;
@@ -34,5 +35,28 @@ public class ReflectionUtilsTest {
                     lookupClass = MethodHandles.Lookup.class.getDeclaredField("lookupClass");
                 }
         );
+    }
+
+    @Test
+    void testChangeFieldType() throws Throwable {
+        class Test {
+            private final boolean booleanField;
+
+            Test() {
+                this.booleanField = true;
+            }
+        }
+
+        {
+            final Test testObj = new Test();
+            final MethodHandle typeSetter = ReflectionUtils.findSetter(Field.class, "type", Class.class);
+            final MethodHandle rootGetter = ReflectionUtils.findGetter(Field.class, "root", Field.class);
+            final Field booleanFieldCopy = Test.class.getDeclaredField("booleanField");
+            final Field booleanField = (Field) rootGetter.bindTo(booleanFieldCopy).invoke();
+            typeSetter.bindTo(booleanField).invoke(String.class);
+            booleanField.set(testObj, "Such boolean");
+            System.out.println(booleanField.get(testObj));
+            System.out.println(testObj.booleanField);
+        }
     }
 }
